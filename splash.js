0fac9dee16f4e207391727fc4fccb79659ec3664
Daneeeboy <dchughes@me.com>
2014-07-15 17:17:03 +0100

    tada
    do this

--- splash.js
+++ splash.js
@@ -6,13 +6,21 @@ $(document).ready(function() {
 	var new_src = "";
 	
 	function isRetina(){
-		return ((window.matchMedia && (window.matchMedia('only screen and (min-resolution: 192dpi), only screen and (min-resolution: 2dppx), only screen and (min-resolution: 75.6dpcm)').matches || window.matchMedia('only screen and (-webkit-min-device-pixel-ratio: 2), only screen and (-o-min-device-pixel-ratio: 2/1), only screen and (min--moz-device-pixel-ratio: 2), only screen and (min-device-pixel-ratio: 2)').matches)) || (window.devicePixelRatio && window.devicePixelRatio > 2)) && /(iPad|iPhone|iPod)/g.test(navigator.userAgent);
+		if (window.matchMedia) {
+            var mq = window.matchMedia("only screen and (min--moz-device-pixel-ratio: 1.3), only screen and (-o-min-device-pixel-ratio: 2.6/2), only screen and (-webkit-min-device-pixel-ratio: 1.3), only screen  and (min-device-pixel-ratio: 1.3), only screen and (min-resolution: 1.3dppx)");
+            if (mq && mq.matches || (window.devicePixelRatio > 1)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
 	}
 	
 	var img_width = "";
 	var img_height = "";
 	
 	if(isRetina()) {
+		alert("HIYA");
 		$(document).find('img.mobile').each(function() {
 			img_src = $(this).attr('src');
 			new_src = $(this).attr('data-retina');
@@ -25,45 +33,75 @@ $(document).ready(function() {
 		});
 	}
 	
-	$(".region").on("click touchstart", function(event) {
-		event.preventDefault();
+	$(".region").on("click", function(event) {
 		var target = $(event.target);
 		if(target.is("a")) {;
 			// do nothing
-		} else if (target.is("img")) { 
-			if(target.hasClass("ontouch")) {
-				if(isRetina()) {
-					new_src = target.attr('data-retina-on');
-				} else {
-					new_src = target.attr('data-on');	
+		} else if($(".mobile").is(":visible")) {
+			$(".region").removeClass("on");
+			$(this).addClass("on");
+		    if (target.is("img")) { 
+				if(target.hasClass("ontouch")) {
+					if(isRetina()) {
+						new_src = target.attr('data-retina-on');
+					} else {
+						new_src = target.attr('data-on');	
+					}
+					
+					target.attr('src', new_src);
 				}
-				
-				target.attr('src', new_src);
-				target.height("100%");
-				target.width("100%");
 			}
 		} else {
+			event.preventDefault();
 			window.location.href = $(this).attr("data-destination");
 		}
 	});
 	
+	$(".arrow").on("touchstart", function(event) {
+		event.preventDefault();
+		window.location.href = $(this).parent().parent().attr("data-destination");
+	});
+	
+	$(".rollover").on("click", function(){
+		img_src = $(this).attr('src'); 
+		if(isRetina()) {
+			new_src = $(this).attr('data-retina-rollover');
+		} else {
+			new_src = $(this).attr('data-rollover'); 
+		}
+		$(this).attr('src', new_src); 
+		$(this).attr('data-rollover', img_src);
+	});
+	
 	$(".rollover").hover(function(){
 		img_src = $(this).attr('src'); 
-		new_src = $(this).attr('data-rollover'); 
+		if(isRetina()) {
+			new_src = $(this).attr('data-retina-rollover');
+		} else {
+			new_src = $(this).attr('data-rollover'); 
+		}
 		$(this).attr('src', new_src); 
 		$(this).attr('data-rollover', img_src);
 	},
 	
 	function(){
 		$(this).attr('src', img_src);
-		$(this).attr('data-rollover', new_src); 
+		if(isRetina()) {
+			$(this).attr('data-retina-rollover', new_src); 
+		} else {
+			$(this).attr('data-rollover', new_src); 
+		}
 	});
 	
 	var cache = new Array();
 	
 	$(".rollover").each(function(){
 		var cacheImage = document.createElement('img');
-		cacheImage.src = $(this).attr('data-rollover');
+		if(isRetina) {
+			cacheImage.src = $(this).attr('data-retina-rollover');
+		} else {
+			cacheImage.src = $(this).attr('data-rollover');
+		}
 		cache.push(cacheImage);
 	});
 	
